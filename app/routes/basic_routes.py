79f031606f77af4226efad2a03430d571285031e
Anantha <ananthashayan2802@gmail.com>
2026-05-28 01:46:28 +0530

    Improved post method and get method with path parameter

--- app/routes/basic_routes.py
+++ app/routes/basic_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from fastapi.params import Body
 from app.schema.schema import AddToCart
-
+from random import randint
 
 router = APIRouter()
 
@@ -13,31 +13,40 @@ def root():
 	}
 
 
-cart = {
-	1 :	{
-			"item_name" : "Earpods",
-			"quantity" : 2
-		},
-	2 :	{
-			"item_name" : "Mobile",
-			"quantity" : 1
-		}
-}
+cart = [
+	{
+		"item_name" : "Earpods",
+		"quantity" : 2,
+		"id" : 1
+	},
+	{
+		"item_name" : "Mobile",
+		"quantity" : 1,
+		"id" : 2
+	}
+]
 
 @router.get('/cart')
 def show_cart():
-	return cart.items()
+	return cart
 
 @router.get('/cart/{item_id}')
 def get_cart_items(item_id: int):
-	return cart.get(item_id)
+	for i in cart:
+		if i["id"] == item_id:
+			return i
+	return "Item not in cart"
+	
+def add_to_cart(item):
+	item_dict = item.dict()
+	item_dict["id"] = randint(1,100000)
+	cart.append(item_dict)
 
 # Request body
 @router.post('/cart')
 def add_item_to_cart(item : AddToCart):	# Extracts all the fields from the Body -> Convert to python dict -> store in 'new'
-	if item.item_name.lower() in cart.items():
-		return "Item exists in cart"
-	item_name = item.item_name
-	item = item.dict()
-	cart[len(cart)+1] = item
-	return (f"{item_name} added to cart!")
+	for i in cart:
+		if item.item_name.lower() == i["item_name"]:
+			return "Item exists in cart"
+	add_to_cart(item)
+	return (f"{item.item_name} added to card")
