63f63c2b56b17d99ad755b3cb7dcd67def940fb7
Anantha <ananthashayan2802@gmail.com>
2026-05-28 02:01:09 +0530

    Added HTTP status codes

--- app/routes/basic_routes.py
+++ app/routes/basic_routes.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.params import Body
-from app.schema.schema import AddToCart
 from random import randint
+from app.schema.schema import AddToCart
 
 router = APIRouter()
 
@@ -35,9 +35,12 @@ def get_cart_items(item_id: int):
 	for i in cart:
 		if i["id"] == item_id:
 			return i
-	return "Item not in cart"
+	raise HTTPException(status_code=404, detail="Item not in cart!")
 	
 def add_to_cart(item):
+	for i in cart:
+		if item.item_name.lower() == i["item_name"].lower():
+			raise HTTPException(status_code=409, detail="Item already in cart")
 	item_dict = item.dict()
 	item_dict["id"] = randint(1,100000)
 	cart.append(item_dict)
@@ -45,8 +48,7 @@ def add_to_cart(item):
 # Request body
 @router.post('/cart')
 def add_item_to_cart(item : AddToCart):	# Extracts all the fields from the Body -> Convert to python dict -> store in 'new'
-	for i in cart:
-		if item.item_name.lower() == i["item_name"]:
-			return "Item exists in cart"
 	add_to_cart(item)
-	return (f"{item.item_name} added to card")
+	# Never raise HTTPException for success code. Use return only
+	# raise HTTPException(status_code=201, detail=f"{item.item_name} added to cart") 
+	return {"message": "Item added to cart"}
