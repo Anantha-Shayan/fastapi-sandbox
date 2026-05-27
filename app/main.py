e91f874d472cb18b314758e68215860ce064928a
Anantha <ananthashayan2802@gmail.com>
2026-05-27 15:09:11 +0530

    Path param

--- app/main.py
+++ app/main.py
@@ -3,10 +3,4 @@ from app.routes.basic_routes import router
 
 app = FastAPI()
 
-@app.get('/')
-def root():
-	return {
-		"message":"/docs for Swagger"
-	}
-
 app.include_router(router)
--- app/routes/basic_routes.py
+++ app/routes/basic_routes.py
@@ -7,3 +7,20 @@ def hello():
 	return {
 		"message":"Hello!"
 	}
+
+items_list = {
+	1: "Bat",
+	2: "Ball"
+}
+
+@router.get('/items')
+def items():
+	return items_list.items()
+
+@router.get('/items/{item_id}')
+def get_items(item_id: int):
+	return items_list.get(item_id)
+
+@router.post('/items/{new}')
+def add_item(new : str):
+	return (f"Updated item list. Added {new}")
