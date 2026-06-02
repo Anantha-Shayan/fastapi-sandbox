e977e9e20ca7c81bfbc4f2dbc3799a063a645a8f
Anantha <ananthashayan2802@gmail.com>
2026-06-02 19:22:26 +0530

    CRUD with DB

--- app/db/database.py
+++ app/db/database.py
@@ -1,5 +1,6 @@
 import os
 import psycopg
+from app.schema.schema import AddToCart
 
 DATABASE_USER = os.getenv("POSTGRES_USER")
 DATABASE_PASSWORD = os.getenv("POSTGRES_PASSWORD")
@@ -13,8 +14,29 @@ def get_connection():
         user=DATABASE_USER,
         password=DATABASE_PASSWORD)
 
-def show_cart(): # Opening with 'with' ensures connection and cursor is closed
+def get_cart(): # Opening with 'with' ensures connection and cursor is closed
     with get_connection() as conn:
         with conn.cursor(row_factory=psycopg.rows.dict_row) as cur:
             cur.execute("SELECT * FROM cart")
             return cur.fetchall()
+
+def get_cart_item(id):
+    with get_connection() as conn:
+        with conn.cursor(row_factory=psycopg.rows.dict_row) as cur:
+            cur.execute(f"SELECT * FROM cart WHERE id={id}")
+            return cur.fetchone()
+
+def add_to_cart(item):
+    with get_connection() as conn:
+        with conn.cursor(row_factory=psycopg.rows.dict_row) as cur:
+            cur.execute(f"INSERT INTO cart(item_name, quantity) values('{item.item_name}', {item.quantity})")
+
+def delete_from_cart(item_name):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(f"DELETE FROM cart WHERE item_name='{item_name}'")
+
+def clear_cart():
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute("DELETE FROM cart")
--- app/routes/basic_routes.py
+++ app/routes/basic_routes.py
@@ -14,49 +14,54 @@ def root():
 
 
 @router.get('/cart')
-def display_cart():
-	return show_cart()
+def show_cart():
+	cart = get_cart()
+	# print(type(cart))
+	if cart:
+		return cart
+	raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cart Empty!")
+
 
 @router.get('/cart/{item_id}')
-def get_cart_item(item_id: int):
-	for i in cart:
-		if i["id"] == item_id:
-			return i
+def get_item_from_cart(item_id: int):
+	item = get_cart_item(item_id)
+	if item:
+		return item
 	raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not in cart!")
 	
-
-def add_to_cart(item):
-	for i in cart:
-		if item.item_name.lower() == i["item_name"].lower():
-			raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Item already in cart")
-	item_dict = item.dict()
-	item_dict["id"] = max([i["id"] for i in cart], default=0) + 1 #default 0 for empty cart
-	cart.append(item_dict)
-
 
 # Request body
 @router.post('/cart', status_code=status.HTTP_201_CREATED) # Default will be 200 which is not ideal status 
 #code when created something. Hence change the default to 201
 def add_item_to_cart(item : AddToCart):	# Extracts all the fields from the Body -> Convert to python dict -> store in 'new'
+	current_cart = get_cart()
+	for i in current_cart:
+		if i["item_name"] == item.item_name:
+			raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Item exists in cart!")
 	add_to_cart(item)
 	return {"message": f"{item.item_name} added to cart"}
 
 
-def delete_item_from_cart(item_id):
-	for i in cart:
-		if i["id"] == item_id:
-			cart.remove(i)
-			return {"message" :"Item removed from cart"}
-	raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not in cart!")
+# def delete_item_from_cart(item_id):
+# 	for i in cart:
+# 		if i["id"] == item_id:
+# 			cart.remove(i)
+# 			return {"message" :"Item removed from cart"}
+# 	raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not in cart!")
 	
 
-@router.delete('/cart/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_item(id : int):
-	delete_item_from_cart(id)
+@router.delete('/cart/{item_name}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_item(item_name: str):
+	current_cart = get_cart()
+	for i in current_cart:
+		if i["item_name"] == item_name:
+			delete_from_cart(item_name)
+			return {"message": "Item removed from cart"}
+	raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not in cart!")
 
 @router.delete('/cart', status_code=status.HTTP_204_NO_CONTENT)
-def clear_cart():
-	cart.clear()
+def clear_cart_items():
+	clear_cart()
 
 @router.patch('/cart/{item_id}')
 def update_quantity(item_id: int, item: UpdateCart):
