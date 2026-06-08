8b7d0dbf79b2c4c47938004138ebbf98d54a4489
Anantha <ananthashayan2802@gmail.com>
2026-06-08 23:56:33 +0530

    Argon hashing added

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,10 +1,17 @@
+from pwdlib import PasswordHash
 from app.db.database import *
 from app.schema.schema import *
 
 class UserAlreadyRegistered(Exception):
     pass
 
+password_hash = PasswordHash.recommended()
+
+def hash_password(pwd):
+    return password_hash.hash(pwd)
+
 def create_new_user(user):
+    user.password = hash_password(user.password)
     try:
         create_user(user)
     except psycopg.errors.UniqueViolation:
